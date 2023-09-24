94238e1414cc12f200d1b68a3a57ee3dc863dea4
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2023-09-24 12:46:13 +0000

    [add] debug1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,15 +3,17 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @books = @user.books
+    @books = Book.all
     @book = Book.new
   end
 
   def index
     @users = User.all
     @book = Book.new
+  end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
