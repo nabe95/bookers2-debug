0c81c5d895a8b8316df5290b9466d2f3a4abd10a
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2023-09-30 08:27:45 +0000

    [add] コメント機能

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -7,8 +7,12 @@ class BookCommentsController < ApplicationController
         redirect_back(fallback_location: root_path)
     end
     
-    private
+    def destroy
+        BookComment.find(params[:id]).destroy
+        redirect_back(fallback_location: root_path)
+    end
     
+    private
     def book_comment_params
         params.require(:book_comment).permit(:comment)
     end
