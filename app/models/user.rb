58219da5b1f54162cffe9fc1d7cca46905a0bbc1
YunaWatanabe <143795237+nabe95@users.noreply.github.com>
2023-10-02 12:26:08 +0000

    [add] フォローuserinfo

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,11 @@ class User < ApplicationRecord
   has_one_attached :profile_image
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  # フォロー
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :following, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
   
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50}
@@ -16,4 +21,20 @@ class User < ApplicationRecord
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
+  
+  #フォローする時
+  def follow(user)
+    relationships.create(followed_id: user.id)
+  end
+  
+  #フォローを外す時
+  def unfollow(user)
+    relationships.find_by(followed_id: user.id).destroy
+  end
+  
+  #フォローの確認
+  def following?(user)
+    following.include?(user)
+  end
+  
 end
